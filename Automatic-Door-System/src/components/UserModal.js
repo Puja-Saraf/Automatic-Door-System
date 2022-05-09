512361c3cc5d9ae8758a7bb87cd13f69db8ce1bc
Puja Saraf <75802818+Puja-Saraf@users.noreply.github.com>
2022-05-09 16:06:26 +0530

    state changes done

--- Automatic-Door-System/src/components/UserModal.js
+++ Automatic-Door-System/src/components/UserModal.js
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './Modal.css'
 
-export default function UserModal({removeConfig}) {
+export default function UserModal({set}) {
+  const [number,setNumber]=useState(1000);
+  const cc=(e)=>{
+    // console.log(e);
+    setNumber(parseInt(e.target.value));
+  }
   return ReactDOM.createPortal((
     <div className="modal-backdrop">
       <div className="user-modal">
         <h3>Set Configuration</h3>
-        <p>duehedpowdawlmkwuheojfem</p>
-        <button onClick={()=>{removeConfig()}}>Done</button>
+        <span>
+        <label>
+          Enter time (in milliseconds)
+        </label>
+        <input type="number"  onChange={cc} />
+        </span>
+        
+        <button onClick={()=>{set(number)}}>Done</button>
       </div>
     </div>
   ), document.body)
